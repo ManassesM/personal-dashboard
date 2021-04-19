7c6246203112ef932dc3a23c5e1e61b127aad161
ManassesM <manamiui2001@gmail.com>
2021-04-19 17:57:18 -0300

    Tabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
+import { NotesComponent } from './notes/notes.component';
+import { TodosComponent } from './todos/todos.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'bookmarks', component: BookmarksComponent },
+  { path: 'todos', component: TodosComponent },
+  { path: 'notes', component: NotesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
