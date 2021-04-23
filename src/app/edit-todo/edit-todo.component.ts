39ffa34051c00297c1db1b06355fcf6b772a29b5
ManassesM <manamiui2001@gmail.com>
2021-04-23 15:44:46 -0300

    Todo Item Edit

--- src/app/edit-todo/edit-todo.component.ts
+++ src/app/edit-todo/edit-todo.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { Todo } from '../shared/todo.model';
+import { TodoService } from '../shared/todo.service';
 
 @Component({
   selector: 'app-edit-todo',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditTodoComponent implements OnInit {
 
-  constructor() { }
+  todo: Todo
+  showValidationErrors: boolean
+
+  constructor(private route: ActivatedRoute, private todoService: TodoService, private router: Router) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      const todoId = paramMap.get('id');
+      this.todo = this.todoService.getTodo(todoId);
+    })
+  }
+
+  onFormSubmit(form: NgForm) {
+    if(form.invalid) return this.showValidationErrors = true
+    
+    this.todoService.updateTodo(this.todo.id, form.value)
+    this.router.navigateByUrl("/todos")
   }
 
 }
