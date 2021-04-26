b6d3bb391380042b4ecad78a9bd55c50959204f7
ManassesM <manamiui2001@gmail.com>
2021-04-26 14:49:08 -0300

    Creating the Manage Bookmarks View

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,10 @@ import { AddBookmarkComponent } from './add-bookmark/add-bookmark.component';
 import { AddNoteComponent } from './add-note/add-note.component';
 import { AddTodoComponent } from './add-todo/add-todo.component';
 import { BookmarksComponent } from './bookmarks/bookmarks.component';
+import { EditBookmarkComponent } from './edit-bookmark/edit-bookmark.component';
 import { EditNoteComponent } from './edit-note/edit-note.component';
 import { EditTodoComponent } from './edit-todo/edit-todo.component';
+import { ManageBookmarksComponent } from './manage-bookmarks/manage-bookmarks.component';
 import { NotesComponent } from './notes/notes.component';
 import { TodosComponent } from './todos/todos.component';
 
@@ -13,7 +15,9 @@ import { TodosComponent } from './todos/todos.component';
 const routes: Routes = [
   { path: 'bookmarks', component: BookmarksComponent, data: { tab: 1 } },
   { path: 'bookmarks/add', component: AddBookmarkComponent },
-  { path: 'bookmarks/add/:id', component: AddBookmarkComponent },
+  { path: 'bookmarks/manage', component: ManageBookmarksComponent, children: [
+    { path: ':id', component: EditBookmarkComponent }
+  ] },
   { path: 'todos', component: TodosComponent, data: { tab: 2 } },
   { path: 'todos/add', component: AddTodoComponent },
   { path: 'todos/:id', component: EditTodoComponent },
